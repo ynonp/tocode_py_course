0cbb02432af636cceef855382348d26beb8e83c1
ynonp <ynonperek@gmail.com>
2020-06-15 20:48:41 +0300

    some comments

--- 01_python_basics/exercises/00_random_nums.py
+++ 01_python_basics/exercises/00_random_nums.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-nums = []
-i = 0
-while len(nums) <10:
-    i = input(f"{len(nums)}) enter a num: ")
-    nums.append(i)
+# You don't need a list here, a simple integer will work just fine
+max_value = float('-inf')
+numbers_read = 0
+while numbers_read < 10:
+    i = int(input(f"{numbers_read}) enter a num: "))
+    if i > max_value:
+        max_value = i
 
-print(f"the biggest num would be {max(nums)}")
+print(f"the biggest num would be {max_value}")
