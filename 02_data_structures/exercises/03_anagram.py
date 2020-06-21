f5c052cc62110402802f9c4675d13e2b0274b7e2
silent-mobius <alex@vaiolabs.com>
2020-06-21 08:55:56 +0300

    addin exercises

---
+++ 02_data_structures/exercises/03_anagram.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+###################################################
+#created by :   Pushtakio
+#purpose    :   
+#date       :
+#version    :   0.0.1
+###################################################
+
+import os.path
+import sys
+
+
+user_input = sys.argv[1:]
+
+
