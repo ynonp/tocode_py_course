5b8e4c840dc9994ece062547c901613c0a8c7171
silent-mobius <alex@vaiolabs.com>
2020-06-21 10:08:33 +0300

    addin exercises

--- 02_data_structures/exercises/03_anagram.py
+++ 02_data_structures/exercises/03_anagram.py
@@ -8,9 +8,23 @@
 ###################################################
 
 import os.path
+import time
 import sys
 
-
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+  
 user_input = sys.argv[1:]
+
+if len(user_input) <= 0:
+    print("please provide full path")
+    time.sleep(2)
+    clear()
+    sys.exit(1)
 
 
